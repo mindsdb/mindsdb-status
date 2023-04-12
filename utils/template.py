683e8b5f0f6fcb5121ce9401e23a0612b980c3a4
zoran <zoran.pandovski@gmail.com>
2023-04-12 16:33:52 +0200

    Add templates

---
+++ utils/template.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+
+class IncidentTemplate():
+
+
+    def get_website_template(self):
+        incident = {
+            "name": "MindsDB Website incident",
+            "message": "We're currently investigating an issue with the MindsDB website",
+            "components": ["cl8nll9g0106225olofc95s0wcm"],
+            "status": "INVESTIGATING",
+            "started": str(datetime.now()),
+            "notify": True,
+            "statuses": [
+                {
+                    "id": "cl8nll9g0106225olofc95s0wcm",
+                    "status": "MAJOROUTAGE"
+                }
+            ]
+        }
+        return incident
+    
+    def get_docs_template(self):
+        incident = {
+            "name": "MindsDB Docs incident",
+            "message": "We're currently investigating an issue with the Documentation website",
+            "components": ["cl8nll9g9106235olof6ovwhb7u"],
+            "status": "INVESTIGATING",
+            "started": str(datetime.now()),
+            "notify": True,
+            "statuses": [
+                {
+                    "id": "cl8nll9g9106235olof6ovwhb7u",
+                    "status": "MAJOROUTAGE"
+                }
+            ]
+        }
+        return incident
+    
+    def get_cloud_template(self):
+        incident = {
+            "name": "MindsDB Cloud incident",
+            "message": "We're currently investigating an issue with MindsDB Cloud",
+            "components": ["cl8nll9fr106215olofubk3u09d"],
+            "status": "INVESTIGATING",
+            "started": str(datetime.now()),
+            "notify": True,
+            "statuses": [
+                {
+                    "id": "cl8nll9fr106215olofubk3u09d",
+                    "status": "MAJOROUTAGE"
+                }
+            ]
+        }
+        return incident
