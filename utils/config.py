793d79bdc5b0adcadc6c317e0038bb0b527cf20d
zoran <zoran.pandovski@gmail.com>
2023-05-30 15:48:06 +0200

    Add CH config

--- utils/config.py
+++ utils/config.py
@@ -32,6 +32,14 @@ SINGLESTORE_CONFIG = {
     "database": "mocked_database"
 }
 
+CLICKHOUSE_CONFIG = {
+    "user": "mocked_user",
+    "password": "mocked_password",
+    "host": "mocked_host",
+    "port": "mocked_port",
+    "database": "mocked_database"
+}
+
 SNOWFLAKE_CONFIG = {
     "host": "mocked_host",
     "user": "mocked_user",
