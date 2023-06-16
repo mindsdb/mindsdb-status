4f8116b3f85dcdaec20532b76b28be5a18cf4ca5
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-16 21:52:53 +0530

    implemented the unit tests for the GitHub integration

---
+++ integrations/test_github.py
@@ -0,0 +1,58 @@
+import unittest
+import mysql.connector
+from utils.query_generator import QueryGenerator as query
+from utils.config import MYSQL_API_CONFIG, GITHUB_CONFIG, generate_random_db_name
+from utils.log import setup_logger
+
+
+class TestGitHubConnection(unittest.TestCase):
+    """
+    Test class for testing the GitHub datasource using the MindsDB SQL API.
+    """
+
+    def setUp(self):
+        """
+        Set up the test environment by establishing a connection to the MindsDB SQL API.
+        """
+        self.query_generator = query()
+        self.logger = setup_logger(__name__)
+        try:
+            self.connection = mysql.connector.connect(**MYSQL_API_CONFIG)
+        except mysql.connector.Error as err:
+            self.logger.error('Connection to SQL API Failed')
+            self.logger.exception(err)
+
+    def tearDown(self):
+        """
+        Clean up the test environment by closing the connection to the MindsDB SQL API.
+        """
+        if self.connection.is_connected():
+            self.connection.close()
+
+    def test_connection_established(self):
+        """
+        Test that the connection to the MindsDB SQL API is established.
+        """
+        assert self.connection.is_connected()
+
+    def test_execute_query(self):
+        """
+        Create a new GitHub Datasource.
+        """
+        try:
+            cursor = self.connection.cursor()
+            random_db_name = generate_random_db_name("github_datasource")
+            query = self.query_generator.create_database_query(
+                        random_db_name,
+                        "github",
+                         GITHUB_CONFIG
+                    )
+            cursor.execute(query)
+            cursor.close()
+        except Exception as err:
+            self.logger.exception(err)
+            assert False, f"Error executing query: {err}"
+
+
+if __name__ == "__main__":
+    unittest.main()
