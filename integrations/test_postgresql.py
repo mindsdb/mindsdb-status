358f18352d2815d747ce8d343646d6c2f36d8f5e
zoran <zoran.pandovski@gmail.com>
2023-07-28 13:56:27 +0200

    Add Postgres tests

--- integrations/test_postgresql.py
+++ integrations/test_postgresql.py
@@ -1,9 +1,11 @@
 import unittest
 import mysql.connector
+from mysql.connector import errorcode
 from utils.query_generator import QueryGenerator as query
 from utils.config import  get_value_from_json_env_var, generate_random_db_name
 from utils.log import setup_logger
-
+from utils.instatus import InstatusClient as ins
+from utils.template import IncidentTemplate as template
 
 class TestPostgreSQLConnection(unittest.TestCase):
     """
@@ -15,14 +17,21 @@ class TestPostgreSQLConnection(unittest.TestCase):
         Set up the test environment by establishing a connection
         to the MindsDB SQL API.
         """
+        self.incident = ins()
+        self.template = template()
         self.query_generator = query()
         self.logger = setup_logger(__name__)
         try:
             config = get_value_from_json_env_var('INTEGRATIONS_CONFIG', 'mindsdb_cloud')
             self.connection = mysql.connector.connect(**config)
         except mysql.connector.Error as err:
-            self.logger.error('Connection to SQL API Failed')
-            self.logger.exception(err)
+            cloud_temp = self.template.get_cloud_sql_api_template()
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
 
     def tearDown(self):
         """
@@ -36,7 +45,9 @@ class TestPostgreSQLConnection(unittest.TestCase):
         """
         Test that the connection to the MindsDB SQL API is established
         """
-        assert self.connection.is_connected()
+        if not self.connection.is_connected():
+            cloud_temp = self.template.get_cloud_sql_api_template()
+            self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
 
     def test_execute_query(self):
         """
@@ -54,8 +65,8 @@ class TestPostgreSQLConnection(unittest.TestCase):
             cursor.execute(query)
             cursor.close()
         except Exception as err:
-            self.logger.exception(err)
-            assert False, f"Error executing query: {err}"
+            cloud_temp = self.template.get_integration_template("PostgreSQL", "clg3ro9pj59766bdoia5mkdgdb")
+            self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
 
 
 if __name__ == "__main__":
