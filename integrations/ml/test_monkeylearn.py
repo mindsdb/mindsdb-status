ad6fdfa33896acb8901e077f8be4a5fddae6d0a3
zoran <zoran.pandovski@gmail.com>
2023-09-11 15:10:28 +0200

    Update Monkeylearn and OpenAI

--- integrations/ml/test_monkeylearn.py
+++ integrations/ml/test_monkeylearn.py
@@ -1,51 +1,13 @@
 import unittest
-import mysql.connector
-from mysql.connector import errorcode
 from utils.query_generator import QueryGenerator as query
-from utils.config import  get_value_from_json_env_var, generate_random_db_name
-from utils.log import setup_logger
-from utils.instatus import InstatusClient as ins
-from utils.template import IncidentTemplate as template
+from utils.config import generate_random_db_name
+from integrations.base_test import BaseTest
+from utils.config import generate_random_db_name
 
-class TestMonkeyLearnConnection(unittest.TestCase):
+class TestMonkeyLearnConnection(BaseTest):
     """
     Test class for testing the Monkey Learn ML Engine using the MindsDB SQL API.
     """
-
-    def setUp(self):
-        """
-        Set up the test environment by establishing a connection to the MindsDB SQL API.
-        """
-        self.incident = ins()
-        self.template = template()
-        self.query_generator = query()
-        self.logger = setup_logger(__name__)
-        try:
-            config = get_value_from_json_env_var('INTEGRATIONS_CONFIG', 'mindsdb_cloud')
-            self.connection = mysql.connector.connect(**config)
-        except mysql.connector.Error as err:
-            cloud_temp = self.template.get_cloud_sql_api_template()
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
-
-    def tearDown(self):
-        """
-        Clean up the test environment by closing the connection to the MindsDB SQL API.
-        """
-        if self.connection.is_connected():
-            self.connection.close()
-
-    def test_connection_established(self):
-        """
-        Test that the connection to the MindsDB SQL API is established.
-        """
-        if not self.connection.is_connected():
-            cloud_temp = self.template.get_cloud_sql_api_template()
-            self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
 
     def test_create_ml_engine(self):
         """
