3107e0aa19d5c8699ff2b1af12363b576ba116bf
zoran <zoran.pandovski@gmail.com>
2023-09-14 14:01:33 +0200

    Add timegpt

--- integrations/ml/test_timegpt.py
+++ integrations/ml/test_timegpt.py
@@ -3,27 +3,27 @@ from utils.config import generate_random_db_name
 from integrations.base_test import BaseTest
 
 
-class TestNeuralForecastConnection(BaseTest):
+class TestTimeGPTConnection(BaseTest):
     """
-    Test class for testing the NeuralForecast ML Engine using the MindsDB SQL API.
+    Test class for testing the TimeGPT ML Engine using the MindsDB SQL API.
     """
 
-    def test_create_neuralforecast_engine(self):
+    def test_create_timegpt_engine(self):
         """
-        Create new NeuralForecast ML Engine.
+        Create new TimeGPT ML Engine.
         """
         try:
             cursor = self.connection.cursor()
-            random_db_name = generate_random_db_name("neuralforecast_engine")
+            random_db_name = generate_random_db_name("timegpt_engine")
             query = self.query_generator.create_ml_engine_query(
                         random_db_name,
-                        "neuralforecast",
+                        "timegpt",
                         {}
                     )
             cursor.execute(query)
             cursor.close()
         except Exception as err:
-            cloud_temp = self.template.get_integration_template("Nixtla's NeuralForecast", "clmj4968l67245c7n5f7ynr8y5")
+            cloud_temp = self.template.get_integration_template("Nixtla's TimeGPT", "clmj4dcp768369c7n5vfqriv5r")
             self.incident.report_incident("cl8nll9f7106187olof1m17eg17", cloud_temp)
 
 
